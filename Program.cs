e2f503812057aac457ff69d8b76e8eb912080663
Parampreet30 <A00264414@mycambrian.ca>
2024-12-05 18:31:35 +0000

    new concretecomponent created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DecoratorPttern;
+class Program
+{
+    static void Main(string[] args)
+    {
+        IComponent<string> baseComponent = new ConcreteComponent();
+    
+            IComponent<string> plaindecorator = new PlainDecorator(baseComponent);
+            IComponent<string> uppercasedecorator = new UpperCaseDecorator(baseComponent);
+            IComponent<string> colordecorator = new ColorDecorator(baseComponent);
+
+            Console.WriteLine("Base Component: " + baseComponent.GetText());
+            Console.WriteLine("Plain Decorator: " + plaindecorator.GetText());
+            Console.WriteLine("UpperCase Decorator: " + uppercasedecorator.GetText());
+            Console.WriteLine("Color Decorator: " + colordecorator.GetText());
+
+
+    }
+    
+     }

b69f332c4677d680ee0440d668e0950bfad8f514
Parampreet30 <A00264414@mycambrian.ca>
2024-12-05 18:00:40 +0000

    Decorator class

---
+++ Decorator.cs
@@ -0,0 +1,16 @@
+namespace DecoratorPttern;
+
+public abstract class Decorator<T> : IComponent<T>
+{
+    protected IComponent<T> component;
+
+    public Decorator(IComponent<T> component)
+    {
+        this.component = component;
+    }
+
+    public virtual T GetText()
+    {
+        return component.GetText();
+    }
+}
